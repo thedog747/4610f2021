d9a2086a9c697a39ce97ae2f87481f6069f96f4d
John O'Neill <jack_t_oneill@yahoo.com>
2021-11-21 01:29:50 -0500

    HW4 Commit 2
    PART 1 of HW4 complete. Added JQuery UI elements including sliders and tabs along with their prescribed attributes.

--- assignments/hw_4/js/main.js
+++ assignments/hw_4/js/main.js
@@ -1,11 +1,109 @@
+/*
+	File: main.js
+	GUI Assignment: Using the jQuery Plugin/UI with Your Dynamic Table
+	Description: Use JQuery validation and assosiated UI library
+	to verify the veracity of values inputted into the form, and
+	dynamically create the table while 
+	John (Jack) O'Neill, UMass Lowell, John_ONeill@student.uml.edu
+	Copyright (c) 2021 by John. All rights reserved. May be freely copied or
+	excerpted for educational purposes with credit to the author.
+	updated by JO on November 21, 2021 at 1:02 AM
+*/
+
+var tabcount = 0;
 var table = document.getElementById("table_mult");
 var lrow = Math.floor(document.getElementsByName("lrow")[0].value);
 var hrow = Math.floor(document.getElementsByName("hrow")[0].value);
 var lcol = Math.floor(document.getElementsByName("lcol")[0].value);
 var hcol = Math.floor(document.getElementsByName("hcol")[0].value);
 
+//initializes sliders
+//slide function will refresh the table on slide change
+$("#lrow_slider").slider({
+	min: -12,
+	max: 12,
+	slide: function(e, ui) {
+		$("#lrow").val(ui.value);
+		makeTable();
+	}
+});
 
+$("#hrow_slider").slider({
+	min: -12,
+	max: 12,
+	slide: function(e, ui) {
+		$("#hrow").val(ui.value);
+		makeTable();
+	}
+});
 
+$("#lcol_slider").slider({
+	min: -12,
+	max: 12,
+	slide: function(e, ui) {
+		$("#lcol").val(ui.value);
+		makeTable();
+	}
+});
+
+$("#hcol_slider").slider({
+	min: -12,
+	max: 12,
+	slide: function(e, ui) {
+		$("#hcol").val(ui.value);
+		makeTable();
+	}
+});
+
+//initializes tabs
+$("#tabs").tabs();
+
+makeTable();
+
+newTab();
+
+//when a new tab is created, jquery arbitrarily stores data inside of it for later use
+function newTab() {
+	tabcount = tabcount + 1;
+	var new_tab = "<li class='ui-state-default ui-corner-top ui-state-active' id='tab-" + tabcount + "'><a>(" + lrow + "," + hrow + ")x(" + lcol + "," + hcol + ")</a><span class='ui-icon ui-icon-close'</span></li>";
+	$("#tabs ul").append(new_tab);
+	$('#tab-' + tabcount + '').data("lrow", lrow);
+	$('#tab-' + tabcount + '').data("hrow", hrow);
+	$('#tab-' + tabcount + '').data("lcol", lcol);
+	$('#tab-' + tabcount + '').data("hcol", hcol);
+	makeClickable();
+}
+
+//called every time a new tab is created, makes a tab click set input values and rebuild the table
+function makeClickable(){
+	$("#tabs ul li").on("click", function() {
+		document.getElementsByName("lrow")[0].value = $(this).data("lrow");
+		document.getElementsByName("hrow")[0].value = $(this).data("hrow");
+		document.getElementsByName("lcol")[0].value = $(this).data("lcol");
+		document.getElementsByName("hcol")[0].value = $(this).data("hcol");
+		syncSlider()
+		makeTable();
+	});
+	//allows the user to individually close all tabs
+	$(".ui-icon-close").on("click", function() {
+		$(this).closest("li").remove();
+    });
+}
+
+//a button that clears multiple tabs, or to be more precise, all at once
+function clearTabs(){
+	$("#tabs ul li").remove();
+}
+
+//synchronizes the slider position with the input value
+function syncSlider(){
+		$("#lrow_slider").slider("value", document.getElementsByName("lrow")[0].value);
+		$("#hrow_slider").slider("value", document.getElementsByName("hrow")[0].value);
+		$("#lcol_slider").slider("value", document.getElementsByName("lcol")[0].value);
+		$("#hcol_slider").slider("value", document.getElementsByName("hcol")[0].value);
+}
+
+//creates the table based on input values
 function makeTable() {
 	
 	validate();
@@ -16,14 +114,22 @@ function makeTable() {
 		
 	}
 	
+	//converts any possible float input to a comparable integer value
 	lrow = Math.floor(document.getElementsByName("lrow")[0].value);
 	hrow = Math.floor(document.getElementsByName("hrow")[0].value);
 	lcol = Math.floor(document.getElementsByName("lcol")[0].value);
 	hcol = Math.floor(document.getElementsByName("hcol")[0].value);
 	
-	if ((lrow > hrow) || (lcol > hcol)){
-		document.getElementById("debug5").innerHTML = "Invalid table: Low bound cannot exceed high bound";
-		return;
+	//new code allows for high end to be less than low end (by way of swapping their values)
+	if (lrow > hrow){
+		let temp = lrow;
+		lrow = hrow;
+		hrow = temp;
+	}
+	if (lcol > hcol){
+		let temp = lcol;
+		lcol = hcol;
+		hcol = temp;
 	}
 	
 	//populate row&column range debug output
@@ -50,6 +156,7 @@ function makeTable() {
 	}
 }
 
+//code for creating rows of the table
 function createRow(hrow, lrow, mult){
 		var row = table.insertRow();
 	var i;
@@ -68,11 +175,7 @@ function createRow(hrow, lrow, mult){
 	}
 }
 
-$.validator.addMethod('loe',
-	function(value, element, params){
-		return (value <= $(params).val());
-	});
-
+//makes sure that the form is valid
 function validate(){
 
 	$("#mform").validate({
@@ -84,7 +187,6 @@ function validate(){
 				min: -12,
 				max: 12,
 				required: true,
-				loe: "#hrow"
 			},
 			
 			hrow: {
@@ -99,7 +201,6 @@ function validate(){
 				min: -12,
 				max: 12,
 				required: true,
-				loe: "#hcol"
 			},
 			
 			hcol: {
@@ -116,7 +217,6 @@ function validate(){
 				min: "Min value error: Lowest value is -12.",
 				max: "Max value error: Highest value is 12.",
 				required: "Field required.",
-				loe: "Low bound cannot exceed high bound."
 			},
 			hrow: {
 				number: "Type error: Number value required.",
@@ -129,7 +229,6 @@ function validate(){
 				min: "Min value error: Lowest value is -12.",
 				max: "Max value error: Highest value is 12.",
 				required: "Field required.",
-				loe: "Low bound cannot exceed high bound."
 			},
 			hcol: {
 				number: "Type error: Number value required.",
